d9c3fa033402425b80e5e1508afbad98b9e54a06
Mnhun <machnhungg@gmail.com>
2022-02-20 03:14:52 +0700

    MN

---
+++ quiz/script.js
@@ -0,0 +1,108 @@
+const startButton = document.getElementById('start-btn')
+const nextButton =document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement=document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+let shuffledQuestions, currectQuestionIndex;
+let quizScore=0;
+
+startButton.addEventListener('click',startGame)
+nextButton.addEventListener('click',()=>{
+    currectQuestionIndex++
+    setnextQuestion()
+})
+
+function startGame(){
+    startButton.classList.add('hide')
+    shuffledQuestions=questions.sort(() =>Math.random()-0.5)
+    currectQuestionIndex=0;
+    questionContainerElement.classList.remove('hide')
+    setnextQuestion()
+    quizScore=0
+}
+
+function setnextQuestion(){
+    resetStart();
+    showQuestion(shuffledQuestions[currectQuestionIndex])
+}
+
+function showQuestion(question){
+    questionElement.innerText=question.question;
+    question.answers.forEach((answer) => {
+        const button = document.createElement('button')
+        button.innerText=answer.text;
+        button.classList.add('btn')
+        if(answer.correct){
+            button.dataset.correct=answer.correct
+        }
+        button.addEventListener('click',selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetStart(){
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while(answerButtonsElement.firstChild){
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e){
+    const selectedButton=e.target
+    const correct= selectedButton.dataset.correct
+
+    setStatusClass(document.body,correct)
+    Array.from(answerButtonsElement.children).forEach((button)=>{
+        setStatusClass(button,button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currectQuestionIndex +1){
+        nextButton.classList.remove("hide")}
+    else{
+        startButton.innerText="restart"
+        startButton.classList.remove("hide")
+    }
+    if (selectedButton.dataset = correct){
+        quizScore++
+    }
+    document.getElementById('right-answers').innerText=quizScore
+}
+
+function setStatusClass(element,correct){
+    clearStatusClass(element)
+    if(correct){
+        element.classList.add("correct")}
+    else{
+        element.classList.add("wrong")}
+}
+function clearStatusClass(element){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+const questions= [
+    {
+        question:'which one of these is a JavaScript framework?',
+        answers:[
+            {text: 'Python',correct:false},
+            {text: 'Django',correct:false},
+            {text: 'React',correct:true},
+            {text: 'Eclipse',correct:false}
+        ],
+    },
+    {
+        question:'who is the prime minister of India?',
+        answers:[
+            {text: 'Narendra Modi',correct:true},
+            {text: 'Rahul Gandhi',correct:false},
+        ],
+    },
+    {
+        question:'what is birthday of MN?',
+        answers:[
+            {text: '15/10',correct:true},
+            {text: '19/11',correct:false},
+            {text: '22/03',correct:false},
+            {text: '21/10',correct:false}
+        ],
+    },
+]
